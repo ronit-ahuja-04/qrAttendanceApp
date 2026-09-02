beb0b04b423a7412513f0f9acdc95f5c9d5f5ff5
Ronit Ahuja <ahuja.ronit.0510@gmail.com>
2026-09-02 20:27:46 +0530

    fix: add missing imports for SharedPreferences

--- app/lib/screens/generate_report_screen.dart
+++ app/lib/screens/generate_report_screen.dart
@@ -1,3 +1,5 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import '../theme/app_colors.dart';
 import '../theme/app_text_styles.dart';
