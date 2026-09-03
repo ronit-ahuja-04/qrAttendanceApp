4bca9d406c636288ce2e96fabed077944688a332
Ronit Ahuja <ahuja.ronit.0510@gmail.com>
2026-09-03 21:19:10 +0530

    build(web): latest flutter web build

--- app/build/web/flutter_bootstrap.js
+++ app/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2556240943" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2132004731" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
