48a276ffe5d36cdba184c8020a5a5da8e600bae8
Ronit Ahuja <ahuja.ronit.0510@gmail.com>
2026-09-03 22:25:31 +0530

    build(web): fix typo and update UI fixes

--- app/build/web/flutter_bootstrap.js
+++ app/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1352197438" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "70348495" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
