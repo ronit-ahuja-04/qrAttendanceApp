86215dc6aecf001352416923a289cf6e96af9dee
Ronit Ahuja <ahuja.ronit.0510@gmail.com>
2026-09-08 03:40:32 +0530

    fix(profile): fix syntax error in row widget

--- app/lib/screens/student_profile_screen.dart
+++ app/lib/screens/student_profile_screen.dart
@@ -294,6 +294,7 @@ class _DigitalIdCard extends StatelessWidget {
                                   ),
                                 ),
                               ],
+                            ),
                           ],
                         ),
                       ),
